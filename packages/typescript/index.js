180c3811a1fcf1d9ba49427abc6596cade89d2a9
hd996 <houdong111@gmail.com>
2022-08-12 17:39:24 +0800

    feat: add some ts rules

--- packages/typescript/index.js
+++ packages/typescript/index.js
@@ -2,7 +2,7 @@ module.exports = {
   env: {
     es6: true,
     browser: true,
-    node: true
+    node: true,
   },
   ignorePatterns: [
     '*.min.*',
@@ -18,20 +18,23 @@ module.exports = {
     '*.yml',
     '*.lock',
     '__snapshots__',
-    '!.*lintrc.js'
+    '!.*lintrc.js',
   ],
   parser: '@typescript-eslint/parser',
   extends: [
     'standard',
-    'plugin:@typescript-eslint/recommended'
+    'plugin:@typescript-eslint/recommended',
   ],
   rules: {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/comma-dangle': ['error', 'always-multiline'],
+    '@typescript-eslint/object-curly-spacing': ['error', 'always'],
     'comma-dangle': 'off',
+    'object-curly-spacing': 'off',
     'no-use-before-define': 'off',
-    'space-before-function-paren': ['error', 'never']
-  }
+    'space-before-function-paren': ['error', 'never'],
+  },
 }
